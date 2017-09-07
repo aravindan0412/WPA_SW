eadf3766ffe6563002c295aa81e497532c55c9e8
aravindan0412 <aravindan0412@gmail.com>
2017-09-07 10:10:24 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.get('/',function(req,res){
+res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use(express.static(__dirname + 'public'));
+
+app.listen(3000);
+console.log("Your IP is running at 3000");
---
+++ sw.js
@@ -0,0 +1,52 @@
+
+//we must change the cacheName to see our changes to app.js ;)
+var cacheName = "simpleStartpageCacheV3"; //we define an cacheName to identify the cache and the version
+//here we add all the files we want later to cache
+var filesToCache=[
+    '/js/app.js',
+    '/images/download.jpg',
+    '/images/flow1.jpg',
+    '/images/flow2.jpg',
+    '/images/flow3.jpg',
+    '/images/flow4.jpg',
+    '/images/rabbit.jpg',
+    '/manifest.json',
+    '/images/hackIcon.png',
+    '/'
+];
+
+
+//here we start with the event listener
+self.addEventListener('install', function(e) {
+    console.debug('called install!');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    )
+});
+
+self.addEventListener('activate', function(e) {
+    //this is called when the service worker is activated!
+    //its also called when the service worker was updated
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key){
+                if(key !== cacheName) {
+                    //its important to remove the old caches
+                    console.debug("[ServiceWorker] Removing old cache ", key);
+                    return caches.delete(key);
+                }
+            }))
+        })
+    )
+});
+
+self.addEventListener('fetch', function(e) {
+    console.debug('event fetch');
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
